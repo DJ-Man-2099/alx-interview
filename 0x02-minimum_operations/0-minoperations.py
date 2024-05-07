5a8f8e222291f7ad3f35703258b089aa04ff7408
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-08 00:18:20 +0300

    Task 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,34 +5,19 @@ Minimum Operations Task
 from math import sqrt
 
 
-def minOperations(n: int) -> int:
+def minOperations(n: any) -> int:
     """Returns the minimum operations to reach n characters in a text file"""
     if n < 2 or not isinstance(n, int):
         return 0
     elif n == 2:
         return 2
     sum = 0
-    prime = True
     while n % 2 == 0:
-        if prime:
-            prime = False
         sum += 2
-        n /= 2
+        n //= 2
     while n > 1:
-        n_not_changed = True
-        for i in range(int(sqrt(n))+1, 1, -2):
-            # print(i)
+        for i in range(3, n+1, 2):
             if n % i == 0:
-                if prime:
-                    prime = False
-                if n_not_changed:
-                    n_not_changed = False
-                n /= i
                 sum += i
-                break
-        if n_not_changed:
-            sum += n
-            break
-    if prime:
-        return int(n)
+                n //= i
     return int(sum)
