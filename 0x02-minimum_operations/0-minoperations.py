8b42259f3835e453ec044c00f1dfd856230d5f16
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-07 16:27:18 +0300

    Task 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,8 +9,8 @@ def minOperations(n: int) -> int:
     """Returns the minimum operations to reach n characters in a text file"""
     first_prime = 0
     second_prime = 0
-    # make loop quicker:
-
+    if n < 2:
+        return 0
     for i in range(int(sqrt(n))+1, 1, -1):
         if n % i == 0:
             first_prime = i
