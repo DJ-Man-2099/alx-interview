6c74e2186f1c3391ff7892fd7657fd199f708b84
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-14 09:09:41 +0300

    Task 1???!

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,12 +2,6 @@
 """4th Project Module"""
 
 
-import re
-from signal import SIGINT, signal
-import sys
-
-
-status_count = {}
 ip_regex = r'^(\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})'
 dash_regex = r'-'
 date_regex = r'\[(\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}\:\d+\.\d+)\]'
@@ -16,8 +10,6 @@ status_regex = r'(200|301|400|401|403|404|405|500)'
 file_size_regex = r'(\d+)$'
 regexes = [ip_regex, dash_regex, date_regex,
            request_regex, status_regex, file_size_regex]
-total_file_size = 0
-count = 0
 
 
 def print_summary(status_count, total_file_size):
@@ -27,19 +19,28 @@ def print_summary(status_count, total_file_size):
         print(f"{key}: {status_count[key]}")
 
 
-signal(SIGINT, lambda signum, frame: print_summary(
-    status_count, total_file_size))
 if __name__ == "__main__":
-    for line in sys.stdin:
-        matches = re.search(r"{} {} {} {} {} {}".format(*regexes), line)
-        if matches:
-            ip, date, status, file_size = matches.groups()
-            total_file_size += int(file_size)
-            if status in status_count:
-                status_count[status] += 1
-            else:
-                status_count[status] = 1
-            count += 1
-            if count == 10:
-                print_summary(status_count, total_file_size)
-                count = 0
+    import sys
+    import re
+
+    status_count = {}
+    total_file_size = 0
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            matches = re.search(r"{} {} {} {} {} {}".format(*regexes), line)
+            if matches:
+                ip, date, status, file_size = matches.groups()
+                total_file_size += int(file_size)
+                if status in status_count:
+                    status_count[status] += 1
+                else:
+                    status_count[status] = 1
+                count += 1
+                if count == 10:
+                    print_summary(status_count, total_file_size)
+                    count = 0
+    except KeyboardInterrupt:
+        print_summary(status_count, total_file_size)
+        raise
