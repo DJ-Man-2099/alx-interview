81702ca0252de7ae8d95996ffb32468e5a849c88
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-21 16:10:52 +0300

    Task 1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,80 @@
 #!/usr/bin/python3
-"""
-0-UTF-8 Validation
-"""
+""" Task 1 """
 
 
-def validUTF8(data):
-    """
-    Data: a list of integers
-    Return: True if data is a valid UTF-8
-    encoding, else return False
-    """
-    byte_count = 0
+from functools import reduce
+import math
+from typing import List
 
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
-                return False
-        else:
-            if i >> 6 != 0b10:
-                return False
-            byte_count -= 1
-    return byte_count == 0
+
+def len_1(c: int) -> bool:
+    """checks first case"""
+    return c >> 7 == 0b0
+
+
+def other_chars(c: int) -> bool:
+    """checks other cases"""
+    return c >> 6 == 0b10
+
+
+len_2 = [
+    lambda c: c >> 5 == 0b110,
+    *([other_chars]*1)
+]
+
+len_3 = [
+    lambda c: c >> 4 == 0b1110,
+    *([other_chars]*2)
+]
+
+len_4 = [
+    lambda c: c >> 3 == 0b1_1110,
+    *([other_chars]*3)
+]
+
+
+def check_utf(chars: List) -> bool:
+    """checks each char separately"""
+    chars_functions = []
+    size = len(chars)
+    if size == 1:
+        return len_1(chars[0])
+    if size == 2:
+        chars_functions = list(zip(chars, len_2))
+    if size == 3:
+        chars_functions = list(zip(chars, len_3))
+    if size == 4:
+        chars_functions = list(zip(chars, len_4))
+    answers = list(map(lambda c: c[1](c[0]), chars_functions))
+    return reduce(lambda x, y: x and y, answers)
+
+
+def try_to_validate(data: List[int], bytes_in_char: int) -> bool:
+    """tries to validate each case"""
+    number_of_chars = int(math.ceil(len(data)/bytes_in_char))
+    chars = []
+    for i in range(number_of_chars):
+        chars.append(data[i*bytes_in_char:(i+1)*bytes_in_char])
+    for char in chars:
+        result = check_utf(char)
+        if result is False:
+            return False
+
+    return True
+
+
+def validUTF8(data: List[int]) -> bool:
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    if not data:
+        return True
+
+    # Focus on only the Least Significant Bytes
+    data = list(map(lambda c: c & 0b1111_1111, data))
+
+    size = len(data)
+    max_bytes = min(size, 4)
+    for i in range(max_bytes, 0, -1):
+        if try_to_validate(data, i):
+            return True
+
+    return False
