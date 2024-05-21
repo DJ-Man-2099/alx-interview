43fc1fb2b9209ba32400525da579639f024e808b
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-21 12:54:11 +0300

    Task 1, with bit-wise ops?

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,7 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
     for char in data:
-        if char >= 256:
+        if ((char >> 8) | 0) != 0:
             return False
 
     return True
