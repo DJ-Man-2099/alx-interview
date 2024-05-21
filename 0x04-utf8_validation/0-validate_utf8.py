d8678a5e2c09ccd64bcc077def17cc1d184ce1e1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-21 15:10:14 +0300

    ??!!???!

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -66,9 +66,9 @@ def try_to_validate(data: List[int], bytes_in_char: int) -> bool:
 
 def validUTF8(data: List[int]) -> bool:
     """determines if a given data set represents a valid UTF-8 encoding"""
-    data = list(data)
-    max_bytes = len(data) if len(data) < 4 else 4
-    for i in range(max_bytes, 0, -1):
+    # size = len(data)
+    # print(f"max: {min(size, 4)}")
+    for i in range(4, 0, -1):
         if try_to_validate(data, i):
             return True
 
