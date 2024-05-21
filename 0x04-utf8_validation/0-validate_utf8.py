765d6c021831bc99f29bb4303f0151e018bd7385
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-21 12:43:43 +0300

    Task 1

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Task 1 """
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    for char in data:
+        if char >= 256:
+            return False
+
+    return True
