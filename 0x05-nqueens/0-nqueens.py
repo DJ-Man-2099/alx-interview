47857d5ab1db4a46a5ef85dd0a44842b873dd277
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-29 20:52:15 +0300

    Task 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,19 +3,17 @@
 Optional Task 1
 """
 import sys
-from typing import List
-from itertools import combinations
 
 
 class Position():
     """Class for defining Positions"""
 
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x, y):
         """Initialize Position in terms of x and y"""
         self.x = x
         self.y = y
 
-    def is_on_same_diagonal(self, other_pos: 'Position') -> bool:
+    def is_on_same_diagonal(self, other_pos):
         """checks if 2 points on same diagonal"""
         x_difference = self.x - other_pos.x
         y_difference = self.y - other_pos.y
@@ -23,20 +21,20 @@ class Position():
             return self.is_on_same_line(other_pos)
         return abs(x_difference/y_difference) == 1
 
-    def is_on_same_line(self, other_pos: 'Position') -> bool:
+    def is_on_same_line(self, other_pos):
         """checks if 2 points on same line"""
         return self.x == other_pos.x or self.y == other_pos.y
 
-    def __str__(self) -> str:
+    def __str__(self):
         """String Representation"""
         return f"[{self.x}, {self.y}]"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         """String Representation"""
         return f"[{self.x}, {self.y}]"
 
 
-def nqueens(size: str = ""):
+def nqueens(size=""):
     """
     solves the nqueens problem
     """
@@ -55,7 +53,7 @@ def nqueens(size: str = ""):
         print(solution)
 
 
-def get_all_positions(size: int = 0) -> List[Position]:
+def get_all_positions(size=0):
     """
     returns list of all positions
     """
@@ -66,22 +64,44 @@ def get_all_positions(size: int = 0) -> List[Position]:
     return positions
 
 
-def get_possible_solutions(number: int = 0, positions: List[Position] = []
-                           ) -> List[List[Position]]:
+def get_possible_solutions(number=0, positions=[]):
     """
     returns list of possible solutions
     """
     valid_solutions = []
-    # print(indices)
     # create all possible solutions
-    all_solutions = list(combinations(positions, number))
+    # Replace this with built in function
+    all_solutions = get_all_combinations(positions, number)
     for solution in all_solutions:
         if check_if_valid_solution(solution):
             valid_solutions.append(list(solution))
     return valid_solutions
 
 
-def is_not_attacking(pos_1: Position, pos_2: Position) -> bool:
+def get_all_combinations(base=[], size=0):
+    """create all possible solutions"""
+    # size is more than no of elements
+    # only one combination
+    base_size = len(base)
+    if size >= base_size:
+        return [base]
+    combinations = []
+    # base case:
+    # len(base) = 2, size = 1
+    if base_size > 1:
+        for i in range(base_size-size+1):
+            if size == 1:
+                combinations.append(base[i:i+size])
+            else:
+                rest = base[i+1:]
+                temp = [base[i]]
+                for c in get_all_combinations(rest, size - 1):
+                    combinations.append(temp + c)
+
+    return combinations
+
+
+def is_not_attacking(pos_1, pos_2):
     """Check if the 2 positions aren't attacking"""
     is_on_line = pos_1.is_on_same_line(pos_2)
     is_on_diagonal = pos_1.is_on_same_diagonal(pos_2)
@@ -93,7 +113,7 @@ def is_not_attacking(pos_1: Position, pos_2: Position) -> bool:
 
 
 def check_if_valid_solution(
-    solution: List[Position] = []
+    solution=[]
 ) -> bool:
     """
     check if solution is valid
@@ -111,3 +131,4 @@ if __name__ == '__main__':
         sys.exit(1)
     nqueens(sys.argv[1])
     # check_if_valid_solution(valid_solutions[1])
+    # print(get_all_combinations([1, 2, 3, 4], 3))
