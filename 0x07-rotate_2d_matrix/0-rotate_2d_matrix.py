5413a044001d71ef96100e0a206995a278f312f4
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-12 01:33:27 +0300

    Task 1

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ Rotate 2D Matrix """
 
-from typing import List
 
-
-def rotate_2d_matrix(matrix: List):
-	""" Rotate 2D Matrix 90 degrees clockwise """
-	new_matrix = [[matrix[j][i] for j in range(len(matrix[i])-1,-1,-1)]
-			   for i in range(len(matrix))]
-	matrix.clear()
-	matrix.extend(new_matrix)
+def rotate_2d_matrix(matrix):
+    """ Rotate 2D Matrix 90 degrees clockwise """
+    new_matrix = [[matrix[j][i] for j in range(len(matrix[i])-1, -1, -1)]
+                  for i in range(len(matrix))]
+    matrix.clear()
+    matrix.extend(new_matrix)
