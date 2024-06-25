cb799cfb41218b28ef05d8e2c7bbb97344aee9f7
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-26 00:50:13 +0300

    NOW???

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,9 +16,8 @@ def get_perim_single(array, x, y):
     for edge_x, edge_y in edges:
         current_x = x+edge_x
         current_y = y+edge_y
-        x_within_limit = current_x >= 0 and current_x < limit_x
-        y_within_limit = current_y >= 0 and current_y < limit_y
-        if (x_within_limit and y_within_limit
+        if (current_x >= 0 and current_x < limit_x
+            and current_y >= 0 and current_y < limit_y
                 and array[current_x][current_y] == 1):
             perimeter -= 1
 
