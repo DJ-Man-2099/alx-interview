0e8b43e43ceca2a3c2baa41b693a63ada1296163
DJ-Man-2099 <davidmesak@hotmail.com>
2024-07-02 22:17:06 +0300

    Task 1

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Prime Game Module """
+
+MARIA = 0
+BEN = 1
+
+
+def playRound(num):
+    """ determines round winner """
+    turn_player = MARIA
+    numbers = list(range(2, num))
+    while numbers:
+        max_num = numbers[-1]
+        current_prime = numbers[0]
+        if current_prime is not None:
+            while current_prime <= max_num:
+                index = numbers.index(current_prime)
+                if index > -1:
+                    numbers.pop(index)
+                current_prime += current_prime
+        turn_player = BEN if turn_player == MARIA else MARIA
+    return BEN if turn_player == MARIA else MARIA
+
+
+def isWinner(x, nums):
+    """ determines winner """
+    ben_wins = 0
+    maria_wins = 0
+    for i in range(x):
+        winner = playRound(nums[i])
+        if winner == BEN:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    if ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return "Maria"
