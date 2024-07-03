d93e272f73ce54d78aa0df7d0f251548567633de
DJ-Man-2099 <davidmesak@hotmail.com>
2024-07-03 23:43:53 +0300

    Task 1 ?????

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,36 +13,36 @@ def playRound(num):
         max_num = numbers[-1]
         current_prime = current_number = numbers[0]
         while current_number <= max_num:
-            if current_number in numbers:
+            if numbers.count(current_number) > 0:
                 numbers.remove(current_number)
             current_number += current_prime
         turn_player = BEN if turn_player == MARIA else MARIA
     return BEN if turn_player == MARIA else MARIA
 
 
-# def primes(n):
-#     """Return list of prime numbers between 1 and n inclusive
-#        Args:
-#         n (int): upper boundary of range. lower boundary is always 1
-#     """
-#     prime = []
-#     sieve = [True] * (n + 1)
-#     for p in range(2, n + 1):
-#         if (sieve[p]):
-#             prime.append(p)
-#             for i in range(p, n + 1, p):
-#                 sieve[i] = False
-#     return prime
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
     """ determines winner """
-    if x is None or nums is None:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
     ben_wins = 0
     maria_wins = 0
     for i in range(x):
-        # number_of_primes = len(primes(nums[i]))
+        number_of_primes = len(primes(nums[i]))
         winner = playRound(nums[i])
         if winner == BEN:
             # print(number_of_primes % 2 == 0)
