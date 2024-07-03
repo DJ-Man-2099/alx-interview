724d12fe66c726524715deff3891174e199cc4ff
DJ-Man-2099 <davidmesak@hotmail.com>
2024-07-03 23:26:49 +0300

    Task 1 ??????

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,14 +13,26 @@ def playRound(num):
         max_num = numbers[-1]
         current_prime = current_number = numbers[0]
         while current_number <= max_num:
-            try:
-                index = numbers.index(current_number)
-                numbers.pop(index)
-            except Exception:
-                pass
+            if current_number in numbers:
+                numbers.remove(current_number)
             current_number += current_prime
         turn_player = BEN if turn_player == MARIA else MARIA
     return BEN if turn_player == MARIA else MARIA
+
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
@@ -30,10 +42,13 @@ def isWinner(x, nums):
     ben_wins = 0
     maria_wins = 0
     for i in range(x):
+        number_of_primes = len(primes(nums[i]))
         winner = playRound(nums[i])
         if winner == BEN:
+            print(number_of_primes % 2 == 0)
             ben_wins += 1
         else:
+            print(number_of_primes % 2 != 0)
             maria_wins += 1
     if ben_wins > maria_wins:
         return "Ben"
